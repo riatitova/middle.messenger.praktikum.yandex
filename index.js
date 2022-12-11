8e33f079ee2af0d71c019d2b14721650e2f0055e
Maria Titova <mary.titova.1995@gmail.com>
2022-12-12 00:35:23 +0400

    feat: add 404 and 500 pages

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ import { renderChats } from './src/pages/chats/modules/chats/chats.js';
 import { renderUserSettings } from './src/pages/user/modules/user-settings/userSettings.js';
 import { renderLoginPage } from './src/pages/home/modules/login/login.js';
 import { renderRegistrationPage } from './src/pages/home/modules/registration/registration.js';
+import { render404Page } from './src/pages/home/modules/page404/page404.js';
+import { render500Page } from './src/pages/home/modules/page500/page500.js';
 
 (() => {
     const root = document.getElementById('root')
-    root.innerHTML = renderRegistrationPage()
+    root.innerHTML = render500Page()
 })();
---
+++ src/pages/home/modules/page500/page500.js
@@ -0,0 +1,6 @@
+import tmpl from './page500.hbs';
+import './page500.scss';
+
+export const render500Page = () => {
+  return tmpl();
+};

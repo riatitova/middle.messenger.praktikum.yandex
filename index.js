eae22c397ecfc0cfd7acf4af79963845ba6eff8d
Maria Titova <mary.titova.1995@gmail.com>
2022-12-12 00:14:06 +0400

    feat: change login page

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import { renderChats } from './src/pages/chats/modules/chats/chats.js';
 import { renderUserSettings } from './src/pages/user/modules/user-settings/userSettings.js';
+import { renderLoginPage } from './src/pages/home/modules/login/login.js';
 
 (() => {
     const root = document.getElementById('root')
-    root.innerHTML = renderUserSettings()
+    root.innerHTML = renderLoginPage()
 })();

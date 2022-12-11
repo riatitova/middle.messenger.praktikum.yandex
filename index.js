7d029663eb2423d4e98372d52fc65c170b4f8859
Maria Titova <mary.titova.1995@gmail.com>
2022-12-12 00:22:02 +0400

    feat: add registration page

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import { renderChats } from './src/pages/chats/modules/chats/chats.js';
 import { renderUserSettings } from './src/pages/user/modules/user-settings/userSettings.js';
 import { renderLoginPage } from './src/pages/home/modules/login/login.js';
+import { renderRegistrationPage } from './src/pages/home/modules/registration/registration.js';
 
 (() => {
     const root = document.getElementById('root')
-    root.innerHTML = renderLoginPage()
+    root.innerHTML = renderRegistrationPage()
 })();
---
+++ src/pages/home/modules/registration/registration.js
@@ -0,0 +1,6 @@
+import tmpl from './registration.hbs';
+import './registration.scss';
+
+export const renderRegistrationPage = () => {
+  return tmpl();
+};
